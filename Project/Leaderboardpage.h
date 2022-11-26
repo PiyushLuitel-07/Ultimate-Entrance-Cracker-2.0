c3032861384ee77c5324c6c703cf74121f49ebb0
prashant72-git <84792438+prashant72-git@users.noreply.github.com>
2022-11-26 21:34:56 +0545

    LeaderBoard

--- Project/Leaderboardpage.h
+++ Project/Leaderboardpage.h
@@ -43,10 +43,13 @@ namespace Project {
 
 	private: System::Windows::Forms::PictureBox^ pictureBox4;
 
-	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::RichTextBox^ richTextBox1;
+
+
 	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::Windows::Forms::PictureBox^ pictureBox3;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
 
 
 
@@ -67,12 +70,14 @@ namespace Project {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Leaderboardpage::typeid));
 			this->pictureBox4 = (gcnew System::Windows::Forms::PictureBox());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->pictureBox3 = (gcnew System::Windows::Forms::PictureBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox4))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// pictureBox4
@@ -85,37 +90,6 @@ namespace Project {
 			this->pictureBox4->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
 			this->pictureBox4->TabIndex = 17;
 			this->pictureBox4->TabStop = false;
-			// 
-			// button1
-			// 
-			this->button1->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->button1->Cursor = System::Windows::Forms::Cursors::Hand;
-			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->button1->Font = (gcnew System::Drawing::Font(L"Calibri Light", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->button1->ForeColor = System::Drawing::Color::Gold;
-			this->button1->Location = System::Drawing::Point(91, 164);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(169, 50);
-			this->button1->TabIndex = 24;
-			this->button1->Text = L"Main Menu";
-			this->button1->UseVisualStyleBackColor = false;
-			this->button1->Click += gcnew System::EventHandler(this, &Leaderboardpage::button1_Click);
-			// 
-			// richTextBox1
-			// 
-			this->richTextBox1->BackColor = System::Drawing::SystemColors::InactiveCaptionText;
-			this->richTextBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
-			this->richTextBox1->Font = (gcnew System::Drawing::Font(L"Calibri Light", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->richTextBox1->ForeColor = System::Drawing::Color::Gold;
-			this->richTextBox1->Location = System::Drawing::Point(366, 152);
-			this->richTextBox1->Name = L"richTextBox1";
-			this->richTextBox1->ReadOnly = true;
-			this->richTextBox1->Size = System::Drawing::Size(654, 570);
-			this->richTextBox1->TabIndex = 25;
-			this->richTextBox1->Text = L"";
-			this->richTextBox1->TextChanged += gcnew System::EventHandler(this, &Leaderboardpage::richTextBox1_TextChanged);
 			// 
 			// textBox1
 			// 
@@ -147,13 +121,58 @@ namespace Project {
 			this->pictureBox3->TabStop = false;
 			this->pictureBox3->Click += gcnew System::EventHandler(this, &Leaderboardpage::pictureBox3_Click);
 			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->button1->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Calibri Light", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->ForeColor = System::Drawing::Color::Gold;
+			this->button1->Location = System::Drawing::Point(91, 164);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(169, 50);
+			this->button1->TabIndex = 24;
+			this->button1->Text = L"Main Menu";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &Leaderboardpage::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->button2->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Calibri Light", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button2->ForeColor = System::Drawing::Color::Gold;
+			this->button2->Location = System::Drawing::Point(303, 164);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(476, 50);
+			this->button2->TabIndex = 28;
+			this->button2->Text = L"Show Leader Board";
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &Leaderboardpage::button2_Click);
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->AllowUserToAddRows = false;
+			this->dataGridView1->AllowUserToDeleteRows = false;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(303, 220);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->ReadOnly = true;
+			this->dataGridView1->Size = System::Drawing::Size(484, 414);
+			this->dataGridView1->TabIndex = 29;
+			this->dataGridView1->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &Leaderboardpage::dataGridView1_CellContentClick);
+			// 
 			// Leaderboardpage
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(11, 24);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1370, 749);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->pictureBox3);
-			this->Controls->Add(this->richTextBox1);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->pictureBox4);
@@ -166,6 +185,7 @@ namespace Project {
 			this->WindowState = System::Windows::Forms::FormWindowState::Maximized;
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox4))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -185,11 +205,22 @@ private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e
 	
 	
 }
-private: System::Void richTextBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	String^ connString = "Data Source=tcp:uec.database.windows.net;Initial Catalog=uecdb;Persist Security Info=True;User ID=sqluec;Password=L12@sqd3";
+	//Data Source=tcp:uec.database.windows.net;Initial Catalog=uecdb;Persist Security Info=True;User ID=sqluec;Password=***********
+	SqlConnection sqlConn(connString);
+	sqlConn.Open();
+	SqlDataAdapter^ sqlDa = gcnew SqlDataAdapter("SELECT * FROM [Ranks] ORDER BY [Marks Obtained] DESC", % sqlConn);
+	DataTable^ dtable = gcnew DataTable();
+	sqlDa->Fill(dtable);
+
+	dataGridView1->DataSource = dtable;
 
 
 
-
+}
+private: System::Void dataGridView1_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
 }
 };
 }
--- Project/Program.cpp
+++ Project/Program.cpp
@@ -19,110 +19,100 @@ void main(array<String^>^ args)
 	Application::SetCompatibleTextRenderingDefault(false);
 	User^ user = nullptr;
 	Admin^ admin = nullptr;
-	//while (true)
-	//{
-	//		Project::Loginform loginForm;
-	//		Project::Adminloginpage adminPage;
-	//		Project::Developer developerPage;
-	//		Project::Homepage hpage;
-	//		Project::Startpage spage;
-	//		Project::Registerpage registerPage;
-	//		Project::Helppage hepage;
-	//		Project::Leaderboardpage lpage;
-	//		Project::Developer dpage;
-	//		
-	//		loginForm.ShowDialog();
-	//		
-	//while (true) 
-	//	{
-	//		////////////////////first login page///////////
-	//		if (loginForm.switchToRegister)
-	//		{
-	//			registerPage.ShowDialog();
-	//			if (registerPage.switchToLogin) {
-	//				loginForm.ShowDialog();
-	//			}
-	//		}
-	//		
+	while (true)
+	{
+		Project::Loginform loginForm;
+		Project::Adminloginpage adminPage;
+		Project::Developer developerPage;
+		Project::Homepage hpage;
+		Project::Startpage spage;
+		Project::Registerpage registerPage;
+		Project::Helppage hepage;
+		Project::Leaderboardpage lpage;
+		Project::Developer dpage;
 
-	//		//////////////////////////
-	//		if (loginForm.switchtohomepage)
-	//		{
-	//			user = registerPage.user;
-	//			break;
-	//		}
-	//		//////////////////
-	//		/////////////////////////adminlogin/////////////
-	//		if (loginForm.switchToAdminLogin)
-	//		{
-	//			adminPage.ShowDialog();
-	//		while(true){
-	//			
-	//			if (adminPage.switchToLogin = true) {
-	//				loginForm.ShowDialog();
-	//				admin = adminPage.admin;
-	//				break;
-	//			}
-	//			if (developerPage.switchTologout) {
-	//				loginForm.ShowDialog();
-	//				break;
-	//			}
-	//			
-	//			}
-	//		}
+		loginForm.ShowDialog();
 
-	//	}
-	//		//-----------------------Homepage--------------------//	
-	//		while (true) {
-	//			if (loginForm.switchtohomepage)
-	//			{
-	//				user = loginForm.user;
-	//				if (user != nullptr) {
-	//					Project::Homepage hpage(user);
-	//					hpage.ShowDialog();
-	//					if (hpage.switchtostartpage) {
-	//						spage.ShowDialog();
-
-
-	//					}
-	//					if (hpage.switchtohelppage) {
-	//						hepage.ShowDialog();
-	//						if (hepage.switchtomainmenufromhelppage) {
-	//							hpage.ShowDialog();
-	//						}
-
-	//					}
-	//					if (hpage.switchtoleaderboradpage) {
-	//						lpage.ShowDialog();
-	//						if (lpage.switchtomainmenufromleaderboard) {
-	//							hpage.ShowDialog();
-	//							continue;
-	//						}
-	//						else
-	//						{
-	//							MessageBox::Show("Error",
-	//								"Program.cpp", MessageBoxButtons::OK);
-	//						}
-	//					}
-	//					if (hpage.switchToLogin)
-	//					{
-	//						break;
-	//					}
-
-	//				}
-	//			}
-
-	//		}
-	//		continue;
-	//}
-	while (true) {
-		for (int i = 1; i < 90; i++) {
-			
-			Project::Startpage spage;
-			spage.ShowDialog();
-			if (spage.nextclick) {
-				spage.ShowDialog();
+		while (true)
+		{
+			////////////////////first login page///////////
+			if (loginForm.switchToRegister)
+			{
+				registerPage.ShowDialog();
+				if (registerPage.switchToLogin) {
+					loginForm.ShowDialog();
+				}
 			}
+
+
+			//////////////////////////
+			if (loginForm.switchtohomepage)
+			{
+				user = registerPage.user;
+				break;
+			}
+			//////////////////
+			/////////////////////////adminlogin/////////////
+			if (loginForm.switchToAdminLogin)
+			{
+				adminPage.ShowDialog();
+				while (true) {
+
+					if (adminPage.switchToLogin = true) {
+						loginForm.ShowDialog();
+						admin = adminPage.admin;
+						break;
+					}
+					if (developerPage.switchTologout) {
+						loginForm.ShowDialog();
+						break;
+					}
+
+				}
+			}
+
 		}
+		//-----------------------Homepage--------------------//	
+		while (true) {
+			if (loginForm.switchtohomepage)
+			{
+				user = loginForm.user;
+				if (user != nullptr) {
+					Project::Homepage hpage(user);
+					hpage.ShowDialog();
+					if (hpage.switchtostartpage) {
+						spage.ShowDialog();
+
+
+					}
+					if (hpage.switchtohelppage) {
+						hepage.ShowDialog();
+						if (hepage.switchtomainmenufromhelppage) {
+							hpage.ShowDialog();
+						}
+
+					}
+					if (hpage.switchtoleaderboradpage) {
+						lpage.ShowDialog();
+						if (lpage.switchtomainmenufromleaderboard) {
+							hpage.ShowDialog();
+							continue;
+						}
+						else
+						{
+							MessageBox::Show("Error",
+								"Program.cpp", MessageBoxButtons::OK);
+						}
+					}
+					if (hpage.switchToLogin)
+					{
+						break;
+					}
+
+				}
+			}
+
+		}
+		continue;
 	}
 }
